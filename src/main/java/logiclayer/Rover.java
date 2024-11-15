c144db9e2cb79a880727907d35f2cc9470c5df66
Valdis <v.beikmanis@gmail.com>
2024-11-15 11:03:03 +0000

    feat: Rover rotate() method

--- src/main/java/logiclayer/Rover.java
+++ src/main/java/logiclayer/Rover.java
@@ -3,11 +3,35 @@ package logiclayer;
 import logiclayer.enums.CompassDirection;
 import logiclayer.enums.Instruction;
 
+import java.util.concurrent.ConcurrentMap;
+
 public class Rover {
 
     private CompassDirection direction;
 
+    // Constructor for the default direction (e.g. North)
+    public Rover() {
+        this.direction = CompassDirection.N;
+    }
+
     public void rotate(Instruction instruction) {
+        if (instruction == Instruction.L) {
+            switch (direction) {
+                case N -> direction = CompassDirection.W;
+                case W -> direction = CompassDirection.S;
+                case S -> direction = CompassDirection.E;
+                case E -> direction = CompassDirection.N;
+            }
+        } else if (instruction == Instruction.R) {
+            switch (direction) {
+                case N -> direction = CompassDirection.E;
+                case E -> direction = CompassDirection.S;
+                case S -> direction = CompassDirection.W;
+                case W -> direction = CompassDirection.N;
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid instruction: " + instruction);
+        }
     }
 
     public CompassDirection getDirection() {
